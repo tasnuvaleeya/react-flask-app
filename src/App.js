0dc8768deffd27a75db86b6db9f477f38e10fad8
tasnuvaleeya <tasnuva2606@gmail.com>
2020-04-16 21:38:22 +0600

    sending request to flask app and receiving response via fetch and promise

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTimes, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +17,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>The current time is: {currentTimes}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
